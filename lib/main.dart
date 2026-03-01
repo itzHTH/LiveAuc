1289981164ffec9e9cc4d75d69b8427ae5ac061e
Leader <jhuth981@gmail.com>
2026-03-02 01:41:03 +0300

    Init Project With FVM 3.38.9

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:street_auction/street_auction_app.dart';
+
+void main() {
+  runApp(const StreetAuctionApp());
+}
---
+++ lib/street_auction_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StreetAuctionApp extends StatelessWidget {
+  const StreetAuctionApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
