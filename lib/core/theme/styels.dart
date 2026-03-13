09c4b3961c344eea96a4d276993d0b8dbd05f2d0
Huthaifa Mohammed <jhuth981@gmail.com>
2026-03-13 21:44:25 +0300

    Implement Page Model & Onboarding Page , inital the models in Onbaoarding Screen, Create Styles Class For handel TextStyle For System , Use Lottie Package For JSON Animations

---
+++ lib/core/theme/styels.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:street_auction/core/theme/app_color.dart';
+
+class Styels {
+  static TextStyle? font20BlackMedium = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColor.uiBlack,
+  );
+
+  static TextStyle? font14GrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColor.uiGray,
+  );
+}
--- lib/features/onborading/screens/onboarding_screen.dart
+++ lib/features/onborading/screens/onboarding_screen.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:street_auction/core/widgets/animated_slider.dart';
+import 'package:street_auction/features/onborading/models/page_model.dart';
+import 'package:street_auction/features/onborading/widgets/onboarding_page.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  final List<PageModel> pages = const [
+    PageModel(
+      title: 'Discover an Endless World of Auctions',
+      description:
+          'From rare collectibles and electronics to vehicles and real estate. Browse thousands of active listings and find your golden opportunity.',
+      image: 'assets/animations/onboarding_1.json',
+    ),
+    PageModel(
+      title: 'Bid Live. Win Big. Every Second Counts.',
+      description:
+          'Jump into real-time auctions and feel the thrill of every bid. Stay sharp, act fast, and claim the deal before anyone else does.',
+      image: 'assets/animations/onboarding_2.json',
+    ),
+    PageModel(
+      title: 'Win Your First Auction, or Start Selling!',
+      description:
+          'Join our vibrant bidding community today. Snatch the best deals as a buyer, or maximize your profits by listing your own items.',
+      image: 'assets/animations/onboarding_3.json',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +42,28 @@ class OnboardingScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 32.h, horizontal: 16.w),
-          child: const Column(
-            children: [AnimatedSlider(currentPage: 2, totalPages: 3)],
+          child: Column(
+            children: [
+              AnimatedSlider(
+                currentPage: _currentPage,
+                totalPages: pages.length,
+              ),
+              SizedBox(height: 60.h),
+
+              Expanded(
+                child: PageView.builder(
+                  onPageChanged: (index) {
+                    setState(() {
+                      _currentPage = index;
+                    });
+                  },
+                  controller: _pageController,
+                  itemCount: pages.length,
+                  itemBuilder: (context, index) =>
+                      OnboardingPage(pages: pages[index]),
+                ),
+              ),
+            ],
           ),
         ),
       ),
