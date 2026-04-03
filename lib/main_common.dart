9e45d8bd1b35c9948343a7474b4dc5ee3db229c8
Huthaifa Mohammed <jhuth981@gmail.com>
2026-04-03 23:56:36 +0300

    fix: initialize ScreenUtil to ensure proper font rendering in release mode

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:street_auction/core/di/dependency_injection.dart';
 import 'package:street_auction/core/routing/app_router.dart';
 import 'package:street_auction/flavors/flavors_config.dart';
@@ -12,5 +13,9 @@ void mainCommon({
   FlavorConfig(flavor: flavor, appName: appName, baseUrl: baseUrl);
   WidgetsFlutterBinding.ensureInitialized();
   AppDependencyInjection.setupGetIt();
+
+  /// To Fix Fonts not showing on Release Mode
+  ScreenUtil.ensureScreenSize();
+
   runApp(LiveAuctionApp(appRouter: AppRouter()));
 }
