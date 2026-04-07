dbbca1f5c5a8e1fe46ac703ed824e2e596bd00f2
Huthaifa Mohammed <jhuth981@gmail.com>
2026-04-07 11:40:27 +0300

    feat: integrate Firebase Crashlytics for production error reporting only in production

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:street_auction/core/di/dependency_injection.dart';
@@ -17,9 +20,19 @@ void mainCommon({
   AppDependencyInjection.setupGetIt();
 
   if (flavor == Flavor.production) {
+    /// Initialize Firebase
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    /// Pass all uncaught "fatal" errors from the framework to Crashlytics
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+    /// Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
   }
 
   /// To Fix Fonts not showing on Release Mode
